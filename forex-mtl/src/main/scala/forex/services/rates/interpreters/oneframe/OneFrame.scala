7267687e8f7e431eda17421d50d1f62e2e7899a7
Aurelien Somename <aurelien.somename@gmail.com>
2023-03-21 16:26:48 +0100

    wip

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/oneframe/OneFrame.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/oneframe/OneFrame.scala
@@ -9,20 +9,7 @@ import forex.services.rates.errors._
 
 import java.time.{OffsetDateTime, ZoneOffset}
 
-/**
-  * (A) The service returns an exchange rate when provided with 2 supported currencies
-  * (B) The rate should not be older than 5 minutes
-  * (C) The service should support at least 10,000 successful requests per day with 1 API token
-  *
-  * (D) The One-Frame service supports a maximum of 1000 requests per day for any given authentication token.
-  * (E) The One-Frame API [...] One or more pairs per request are allowed.
-  *
-  * - OneFrame service
-  * - Cache service with expiration cache
-  *
-  * - 86400 seconds per day & (C) => 1 query on average, every 8 seconds
-  * - 1440 minutes per day & (B) & (D) & (E) => 1 query every [2, 5[ minutes to the One-Frame API with all pairs
-  */
+
 class OneFrame[F[_]: Applicative](client: Client, cache: Cache, cacheDurationSeconds: Int) extends Algebra[F] {
 
   // arbitrary initial value long enough to force the first update
