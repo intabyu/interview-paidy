eb42fee3e8e345d8157edb3109461ed652cd4014
Aurelien Somename <aurelien.somename@gmail.com>
2023-03-21 16:24:04 +0100

    wip

--- forex-mtl/src/main/scala/forex/domain/Currency.scala
+++ forex-mtl/src/main/scala/forex/domain/Currency.scala
@@ -5,23 +5,14 @@ import cats.Show
 sealed trait Currency
 
 object Currency {
-  
   case object AUD extends Currency
-
   case object CAD extends Currency
-
   case object CHF extends Currency
-
   case object EUR extends Currency
-
   case object GBP extends Currency
-
   case object NZD extends Currency
-
   case object JPY extends Currency
-
   case object SGD extends Currency
-
   case object USD extends Currency
 
   implicit val show: Show[Currency] = Show.show {
